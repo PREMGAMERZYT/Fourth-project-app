914e89f494cfe0abb35b1d07613a1f253aa3fffd
PremCoder <kasturawwashunnamani@gmail.com>
2026-04-20 10:44:06 +0530

    writting the logic to take the name which was entered in first screen to the second screen

--- app/src/main/java/com/example/fourth_project_app/MainActivity.java
+++ app/src/main/java/com/example/fourth_project_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.fourth_project_app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -12,15 +13,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText name;
+    public static final String EXTRA_NAME = "com.example.fourth_project_app.extra.name";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
+        name.findViewById(R.id.editTextText);
+        String nametext = name.getText().toString();
     }
-
     public void opensecondsctivity(View v) {
         Toast.makeText(this, "Openning second activity", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, message_reciver.class);
