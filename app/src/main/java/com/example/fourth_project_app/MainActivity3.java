3c4dc4a7d0ad8056c05f0ebf026fdba76ad87ed2
PremCoder <kasturawwashunnamani@gmail.com>
2026-04-21 22:39:03 +0530

    added the fourth screen and libked to the screen 3 sucussfully

--- app/src/main/java/com/example/fourth_project_app/MainActivity3.java
+++ app/src/main/java/com/example/fourth_project_app/MainActivity3.java
@@ -2,7 +2,9 @@ package com.example.fourth_project_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +12,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity3 extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class MainActivity3 extends AppCompatActivity {
+    public static final String Name_Of_Thired_secren = "com.example.fourth_project_app.name_of_thired_secren";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,10 @@ public class MainActivity3 extends AppCompatActivity {
 
     public void Onclicking(View v) {
         Intent intent = new Intent(this, MainActivity4.class);
+        TextView textView = findViewById(R.id.textView3);
+        String Name22 = textView.getText().toString();
+        intent.putExtra(Name_Of_Thired_secren,Name22);
         startActivity(intent);
+
     }
 }
--- app/src/main/java/com/example/fourth_project_app/MainActivity4.java
+++ app/src/main/java/com/example/fourth_project_app/MainActivity4.java
@@ -1,6 +1,9 @@
 package com.example.fourth_project_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity4 extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
-
+        TextView textView = findViewById(R.id.textView4);
+        Intent intent = getIntent();
+        String Screen3 = intent.getStringExtra(MainActivity3.Name_Of_Thired_secren);
+        textView.setText(Screen3);
     }
 }
