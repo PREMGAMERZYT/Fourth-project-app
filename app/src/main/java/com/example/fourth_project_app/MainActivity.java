1f913c59c3aa0e17951d5391deb81c3e15bca29b
PremCoder <kasturawwashunnamani@gmail.com>
2026-04-25 21:46:52 +0530

    fixed error in the code

--- app/src/main/java/com/example/fourth_project_app/MainActivity.java
+++ app/src/main/java/com/example/fourth_project_app/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.fourth_project_app;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,6 +34,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String UrlText = url.getText().toString();
                 Toast.makeText(MainActivity.this, UrlText, Toast.LENGTH_SHORT).show();
+                Uri webpage = Uri.parse(UrlText);
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+                Intent chooser = Intent.createChooser(intent, "Open With");
+                startActivity(chooser);
+
 
             }
         });
