f587db66fbb4a11741b23cacb01cc8387aa95d65
PremCoder <kasturawwashunnamani@gmail.com>
2026-04-25 21:16:26 +0530

    added The onclick listener on the button using java file

--- app/src/main/java/com/example/fourth_project_app/MainActivity.java
+++ app/src/main/java/com/example/fourth_project_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.fourth_project_app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editText;
+    private Button button;
     public static final String EXTRA_NAME = "com.example.fourth_project_app.extra.name";
     public static final String Second_name = "com.example.fourth_project_app.second.name";
     public static final  String Thired_name = "com.example.fourth_project_app.thired.name";
@@ -23,26 +25,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editText = findViewById(R.id.editTextText);
-    }
-    public void opensecondsctivity(View v) {
-        String Name = editText.getText().toString();
-        if (Name.equals("Prem")) {
-            Intent intent = new Intent(this,MainActivity2.class);
-            intent.putExtra(Second_name,Name);
-            startActivity(intent);
-        } else if (Name.equals("prajwal")) {
-            Intent intent = new Intent(this, message_reciver.class);
-            intent.putExtra(EXTRA_NAME,Name);
-            startActivity(intent);
-        } else if (Name.equals("Three")) {
-            Intent intent = new Intent(this,MainActivity3.class);
-            intent.putExtra(Thired_name,Name);
-            startActivity(intent);
-        } else {
+        button = findViewById(R.id.button);
 
-        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
     }
+
     // learning Implicit intents
     //we can request any other app to perform any action
 }
